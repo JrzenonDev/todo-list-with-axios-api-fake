cef242c47fffde09a5b1792e52fdee2f8d5ae389
JrzenonDev <jrobertoonb@gmail.com>
2022-02-02 01:50:41 -0300

    listing taks in List

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="container mt-2">
-    <b-card title="Titulo">
-      <b-card-text>Descrição</b-card-text>
-
-      <b-button variant="outline-secondary" class="mr-2"> Editar </b-button>
-      <b-button variant="outline-danger"> Excluir </b-button>
-    </b-card>
+    <div v-for="(task, index) in tasks" :key="index">
+      <b-card :title="task.subject" class="mb-2">
+        <b-card-text>{{ task.description }}</b-card-text>
+        <b-button variant="outline-secondary" class="mr-2"> Editar </b-button>
+        <b-button variant="outline-danger"> Excluir </b-button>
+      </b-card>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  created () {
+    this.tasks = (localStorage.getItem('tasks')) ? JSON.parse(localStorage.getItem('tasks')) : []
+  }
+}
+</script>
