23e01b450f4df8b336b1cc9f8d9d87628eb37a71
JrzenonDev <jrobertoonb@gmail.com>
2022-02-03 01:47:22 -0300

    configure axios and vue-api-query in project

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Vuelidate from 'vuelidate'
+import axios from 'axios'
+import { Model } from 'vue-api-query'
 
 Vue.use(BootstrapVue)
 
 Vue.use(Vuelidate)
+
+Model.$http = axios
 
 Vue.config.productionTip = false
 
