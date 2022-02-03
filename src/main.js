b18224f9cc873b3e62648e110fd8d89705db91e4
JrzenonDev <jrobertoonb@gmail.com>
2022-02-03 00:24:25 -0300

    add and configure library vuelidate in project

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import Vuelidate from 'vuelidate'
+
 Vue.use(BootstrapVue)
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
