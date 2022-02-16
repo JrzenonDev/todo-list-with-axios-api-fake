6246d4e3e80fc2f8007a3c52c011f207dfff66c8
JrzenonDev <jrobertoonb@gmail.com>
2022-02-15 23:21:37 -0300

    create component Register

---
+++ src/views/Register.vue
@@ -0,0 +1,158 @@
+<template>
+  <b-row class="vh-100 vw-100 row-login">
+    <b-col sm="7" class="d-flex justify-content-center align-items-center right-register">
+      <img class="img-login" src="../assets/images/create.svg" alt="Login" />
+    </b-col>
+    <b-col sm="5" class="d-flex justify-content-center align-items-center right-register">
+      <div class="col-8">
+        <h2 class="text-center mb-5 title-register">Faça o seu cadastro</h2>
+        <b-form>
+          <b-form-group
+            label="Nome"
+            label-for="name"
+          >
+            <b-form-input
+              id="name"
+              type="text"
+              placeholder="O seu nome"
+              autocomplete="off"
+              v-model.trim="$v.form.name.$model"
+              :state="getValidation('name')"
+            />
+          </b-form-group>
+          <b-form-group
+            label="E-mail"
+            label-for="email"
+          >
+            <b-form-input
+              id="email"
+              type="email"
+              placeholder="seu@email.com.br"
+              autocomplete="off"
+              v-model.trim="$v.form.email.$model"
+              :state="getValidation('email')"
+            />
+          </b-form-group>
+          <b-form-group
+            label="Senha"
+            label-for="password"
+          >
+            <b-form-input
+              id="password"
+              type="password"
+              placeholder="Digite sua senha"
+              v-model.trim="$v.form.password.$model"
+              :state="getValidation('password')"
+            />
+          </b-form-group>
+          <b-form-group
+            label="Confirme sua senha"
+            label-for="confirmPassword"
+          >
+            <b-form-input
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirme a sua senha"
+              v-model.trim="$v.form.confirmPassword.$model"
+              :state="getValidation('confirmPassword')"
+            />
+          </b-form-group>
+          <b-button
+            type="button"
+            variant="primary"
+            block
+            @click="login"
+          >
+            <i class="fas fa-sign-in-alt" /> Cadastrar
+          </b-button>
+          <hr />
+          <b-button
+            type="button"
+            variant="outline-secondary"
+            block
+            @click="register"
+          >
+            <i class="fas fa-arrow-left" /> Voltar
+          </b-button>
+        </b-form>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { required, minLength, email, sameAs } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    }
+  },
+  validations: {
+    form: {
+      name: {
+        required,
+        minLength: minLength(3)
+      },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      confirmPassword: {
+        required,
+        sameAsPassword: sameAs('password')
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$v.$touch()
+      if (this.$v.$error) {
+        return true
+      }
+    },
+    register () {},
+    getValidation (field) {
+      if (this.$v.$dirty === false) {
+        return null
+      }
+      return !this.$v.form[field].$error
+    }
+  }
+}
+</script>
+
+<style>
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+  .row-login {
+    margin-left: 0;
+  }
+  .right-register {
+    background-color: #f2f2f2;
+  }
+  .title-register {
+    font-weight: bold;
+  }
+  .img-login {
+    width: 600px;
+    height: 600px;
+  }
+</style>
